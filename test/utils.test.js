6bfd9c60754f5bc0d192c2e149df777d0e81c515
yamadapc <tacla.yamada@gmail.com>
2014-05-18 14:46:56 -0300

    Improve `lib/utils.js` code coverage.

--- test/utils.test.js
+++ test/utils.test.js
@@ -9,25 +9,77 @@ var should = require('should'),
 describe('utils', function() {
   it('gets exposed', function() { should.exist(utils); });
 
-  describe('.deoopfy(method)', function() {
-    it('returns a new function, binding the first argument as input\'s context', function() {
-      var fn = utils.deoopfy(function() {
-        this.yey = true;
-      });
+  describe('.map(fn, arr)', function() {
+    it('maps out function over the array\'s elements', function() {
+      var arr = [ 1, 2, 3 ];
 
-      var obj = { yey: false };
-      fn(obj);
-      obj.yey.should.be.ok;
+      var result = utils.map(addStrIndex, arr);
+      should.exist(result);
+      result.should.eql([ '0:1', '1:2', '2:3' ]);
+
+      function addStrIndex(el, i) {
+        return i + ':' + el;
+      }
+    });
+  });
+
+  describe('.filter(fn, arr)', function() {
+    it('filters arrays based on a predicate function', function() {
+      var arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+
+      var result = utils.filter(even, arr);
+      should.exist(result);
+      result.should.eql([2, 4, 6, 8, 10]);
+
+      function even(n) {
+        return n % 2 === 0;
+      }
+    });
+  });
+
+  describe('.compose(f, g)', function() {
+    it('composes two functions together', function() {
+      var f = function(name) { return 'Hello ' + name + '!'; };
+      var g = function(name) { return name.toUpperCase(); };
+
+      var fg = utils.compose(f, g);
+      should.exist(fg);
+      fg.should.be.instanceof(Function);
+      var result = fg('robert');
+      result.should.equal('Hello ROBERT!');
+    });
+  });
+
+  describe('.partial(fn, args)', function() {
+    it('partially applies a function to a set of arguments', function() {
+      var add1 = utils.partial(add, 1);
+      should.exist(add1);
+      add1.should.be.instanceof(Function);
+      add1(2).should.equal(3);
+
+      function add(x, y) {
+        return x + y;
+      }
     });
 
-    it('passes on the remaining arguments into the function', function() {
-      var args;
-      var fn = utils.deoopfy(function() {
-        args = Array.prototype.slice.call(arguments);
-      });
+    it('works with spliced arguments', function() {
+      var add1And2 = utils.partial(add, 1, 2);
+      should.exist(add1And2);
+      add1And2.should.be.instanceof(Function);
+      add1And2(3).should.equal(6);
+      function add(x, y, z) {
+        return x + y + z;
+      }
+    });
 
-      fn({}, 1, 2, 3);
-      args.should.eql([1, 2, 3]);
+    it('works with arrays of arguments', function() {
+      var add1And2 = utils.partial(add, [1, 2]);
+      should.exist(add1And2);
+      add1And2.should.be.instanceof(Function);
+      add1And2(3).should.equal(6);
+      function add(x, y, z) {
+        return x + y + z;
+      }
     });
   });
 
